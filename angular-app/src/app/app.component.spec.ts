7670ae51eb53e4796350a23afca5732e4721d71d
Konijima <mathieugermain1991@gmail.com>
2022-08-24 21:41:55 -0400

    Add angular/material

--- angular-app/src/app/app.component.spec.ts
+++ angular-app/src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-app'`, () => {
+  it(`should have as title 'PZPW Desktop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-app app is running!');
+    expect(app.title).toEqual('PZPW Desktop');
   });
 });
