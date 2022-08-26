c16ffbf3f1cfcf7e091cbb9bfbd7af419134e620
Konijima <mathieugermain1991@gmail.com>
2022-08-25 23:04:11 -0400

    Update app-routing.module.ts

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageProjectComponent } from './pages/page-project/page-project.component';
 
 const routes: Routes = [
-  
+  {
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: PageHomeComponent
+  },
+  {
+    path: 'project/:filePath',
+    component: PageProjectComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
 ];
 
 @NgModule({
