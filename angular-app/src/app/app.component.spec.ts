d44f3a5a454589e93b4d8fb54f6e70b5faecf743
Konijima <mathieugermain1991@gmail.com>
2022-08-26 10:13:30 -0400

    Add app component tests

--- angular-app/src/app/app.component.spec.ts
+++ angular-app/src/app/app.component.spec.ts
@@ -4,8 +4,12 @@ import { AppComponent } from './app.component';
 import { ListProjectsComponent } from './components/projects/list-projects/list-projects.component';
 import { LoadingOverlayComponent } from './components/ui/loading-overlay/loading-overlay.component';
 import { MaterialModule } from './material.module';
+import { ProjectManagerService } from './services/project-manager.service';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let service: ProjectManagerService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -18,11 +22,33 @@ describe('AppComponent', () => {
         LoadingOverlayComponent
       ],
     }).compileComponents();
+
+    service = TestBed.inject(ProjectManagerService);
+
+    const fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should call IsInitialized and return false', () => {
+    const result = app.IsInitialized();
+    expect(result).toBeFalse();
+  });
+
+  it('should call IsInitialized and return true', () => {
+    service.Initialized.next(true);
+
+    const result = app.IsInitialized();
+    expect(result).toBeTrue();
+  });
+
+  it('should call GetProjects and return empty array', () => {
+    service.Projects.next([{} as any]);
+
+    const result = app.GetProjects();
+    expect(result).toHaveSize(1);
   });
 });
