0856066b08c5929d3a94d349f32a972fc83eb3cf
Konijima <mathieugermain1991@gmail.com>
2022-08-25 23:04:24 -0400

    Update app.module.ts

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// Components
 import { AppComponent } from './app.component';
+import { LoadingOverlayComponent } from './components/ui/loading-overlay/loading-overlay.component';
+import { ListProjectsComponent } from './components/projects/list-projects/list-projects.component';
+import { ItemProjectComponent } from './components/projects/item-project/item-project.component';
+
+// Pages
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageProjectComponent } from './pages/page-project/page-project.component';
+
+// Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+// Services
+import { ProjectManagerService } from './services/project-manager.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingOverlayComponent,
+    ListProjectsComponent,
+    ItemProjectComponent,
+    PageHomeComponent,
+    PageProjectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [ProjectManagerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
