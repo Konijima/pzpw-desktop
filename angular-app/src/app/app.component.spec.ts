74daea5a793435e52f896cbfaed644f12baa9aa6
Konijima <mathieugermain1991@gmail.com>
2022-08-25 23:04:21 -0400

    Update app component

--- angular-app/src/app/app.component.spec.ts
+++ angular-app/src/app/app.component.spec.ts
@@ -1,28 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ListProjectsComponent } from './components/projects/list-projects/list-projects.component';
+import { LoadingOverlayComponent } from './components/ui/loading-overlay/loading-overlay.component';
+import { MaterialModule } from './material.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MaterialModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ListProjectsComponent,
+        LoadingOverlayComponent
       ],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'PZPW Desktop'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('PZPW Desktop');
   });
 });
