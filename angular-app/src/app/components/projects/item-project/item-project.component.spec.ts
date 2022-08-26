7c65974082683ccc0822e8e6bed11895ad3ebb18
Konijima <mathieugermain1991@gmail.com>
2022-08-26 10:08:00 -0400

    Update item project component

--- angular-app/src/app/components/projects/item-project/item-project.component.spec.ts
+++ angular-app/src/app/components/projects/item-project/item-project.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { MaterialModule } from 'src/app/material.module';
 
 import { ItemProjectComponent } from './item-project.component';
@@ -12,7 +13,8 @@ describe('ItemProjectComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        MaterialModule
+        MaterialModule,
+        RouterTestingModule
       ],
       declarations: [
         ItemProjectComponent
