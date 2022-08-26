097d86db9b35eb5555809a5fc081018e3c973207
Konijima <mathieugermain1991@gmail.com>
2022-08-25 23:05:02 -0400

    Add loading-overlay component

---
+++ angular-app/src/app/components/ui/loading-overlay/loading-overlay.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-overlay',
+  templateUrl: './loading-overlay.component.html',
+  styleUrls: ['./loading-overlay.component.scss']
+})
+export class LoadingOverlayComponent {
+
+  @Input() visible!: boolean;
+
+  constructor() { }
+
+}
